ef533ff16424f2f14785002ad3711383c304ced1
setterman <gartorant@gmail.com>
2025-05-24 21:49:16 +0200

    modificados los puertos del modulo counter

--- new_IP/counter.v
+++ new_IP/counter.v
@@ -1,33 +1,38 @@
 module counter #(   parameter   WIDTH = 32)  
                 (   
-                    input               iclk,               // Signal clock
-                    input               ireset,             // Signal reset
-                    input               ienable,            // Signal enable
-                    input       [WIDTH-1:0]  iload_value,   // End load value
-                    input       [WIDTH-1:0]  threshold,     // Value 
-                    output reg  [WIDTH-1:0]  ocount,        // Count value
-                    output reg  oterminal_count             // End count
+                    input               iclk,               //bit       - Signal clock
+                    input               ireset,             //bit       - Signal reset
+                    input               ienable,            //bit       - Signal enable
+                    input               clearTC_threshold,  //bit       - Signal to clear oTC_threshold (IRQ)
+                    input       [WIDTH-1:0]  iload_value,   //U[31:0]   - End load value
+                    input       [WIDTH-1:0]  threshold,     //U[31:0]   - Value 
+                    output reg  oterminal_count,            //bit       - End count
+                    output reg  oTC_threshold,              //bit       - Signal 
+                    output reg  [WIDTH-1:0]  ocount         //U[31:0]   - Count value
+
                 );
 
-always @(posedge iclk or posedge ireset) //revisar el del juego de vida
+always @(posedge iclk or negedge ireset)
     begin
         if(!ireset)
             begin
                 ocount <= {WIDTH{1'b0}};
                 oterminal_count <= 1'b0;
+                oTC_threshold <= 1'b0;
             end
         else
             begin
                 if(ienable)
                     begin   
-                        if(ocount<iload_value)
+                        if(ocount<(iload_value-1'b1))       //The loop executes one more time than the condition is met.
                             ocount <= ocount+1'b1;
                         else // END COUNTER
                             begin
                                 oterminal_count <= 1'b1;   //Enable final count
                                 ocount <= {WIDTH{1'b0}};
                             end
-                    end                        
+                    end  
+                    //todo: case else ienable = 0                      
             end
     end
 
