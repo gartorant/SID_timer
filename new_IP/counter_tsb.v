854639f57bb1843bc743b891f3f35afd6f11c2f8
setterman <gartorant@gamil.com>
2025-05-25 23:44:02 +0200

    instantiated the UUT in the testbench

--- new_IP/counter_tsb.v
+++ new_IP/counter_tsb.v
@@ -2,4 +2,28 @@
 
 module counter_tsb ();
 
+reg clock;
+reg reset;
+reg enable;
+reg clearTC_count;
+
+localparam counter_value = 1000; // Example counter value
+localparam threshold_value = 500; // Example threshold value
+
+wire TC_COUNT;
+wire TC_THRESHOLD;
+wire [31:0] count_value;
+
+
+counter uut_counter (
+    .iclk(clock),
+    .ireset(reset),
+    .ienable(enable),
+    .clearTC_count(clearTC_count),
+    .iload_value(counter_value),
+    .threshold(threshold_value),
+    .oTC_count(TC_COUNT),
+    .oTC_threshold(TC_THRESHOLD),
+    .ocount(count_value)
+);
 endmodule 
